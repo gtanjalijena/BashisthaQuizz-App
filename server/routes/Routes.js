ae89e19f1ec2d422c54233220410589df493279a
gtanjalijena <157167388+gtanjalijena@users.noreply.github.com>
2024-01-20 02:27:06 +0530

    Update Routes.js
    update 03

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -5,7 +5,12 @@ import isAuthenticated from "../middlewares/jwt.js";
 
 const router = express.Router();
 
-router.get("/", test};
+router.get("/",  async (req, res) => {
+    res.send("test page2");
+  });
+router.get("/xyz",  async (req, res) => {
+    res.send("test page3");
+  });
 router.post("/signup", signupRoute);
 router.post("/login", loginRoute);
 
